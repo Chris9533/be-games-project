2b6466c6d93b8df3e0785fe76510b0a48c0d5938
Chris9533 <christopherbrownhill1995@gmail.com>
2022-05-17 14:49:13 +0100

    added comment_count to review object requested

--- Models/reviews.models.js
+++ Models/reviews.models.js
@@ -1,14 +1,19 @@
 const db = require("../db/connection.js");
 
 exports.fetchReview = (review_id) => {
-    return db.query("SELECT * FROM reviews WHERE review_id = $1", [review_id])
-    .then((result) => {
-      const review = result.rows
+    const review = db.query("SELECT * FROM reviews WHERE review_id = $1", [review_id])
+    const users = db.query("SELECT * FROM comments WHERE review_id = $1", [review_id])
+    return Promise.all([review, users])
+    .then(([reviewData, userData]) => {
+      const commentCount = userData.rows.length
+      const review = reviewData.rows
        
       if(!review.length) {
           return Promise.reject({ status: 404, msg: "review not found"})
       } else {
-       return review[0];
+          review[0].comment_count = commentCount
+
+          return review[0];
       }
 
     })
