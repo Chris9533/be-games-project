7950f14f6a7c208b2cb491797af8161c223c7747
Chris9533 <christopherbrownhill1995@gmail.com>
2022-05-17 10:10:57 +0100

    removed unused code from categories.models

--- Models/categories.models.js
+++ Models/categories.models.js
@@ -8,17 +8,3 @@ exports.fetchCategories = () => {
 
     })
 }
-
-exports.fetchReview = (review_id) => {
-    return db.query("SELECT * FROM reviews WHERE review_id = $1", [review_id])
-    .then((result) => {
-      const review = result.rows
-       
-      if(!review.length) {
-          return Promise.reject({ status: 404, msg: "review not found"})
-      } else {
-       return review;
-      }
-
-    })
-}
