e3e42741689458c0320a4686d7f46f8a3416d9d2
Chris9533 <christopherbrownhill1995@gmail.com>
2022-05-18 15:00:20 +0100

    removed console logs

--- Models/reviews.models.js
+++ Models/reviews.models.js
@@ -33,7 +33,6 @@ exports.updateReview = (review_id, alterVotes) => {
 exports.fetchReviews = () => {
     return db.query("SELECT reviews.review_id, reviews.title, reviews.owner, reviews.review_img_url, reviews.category, reviews.created_at, reviews.votes, COUNT(comments.review_id)::INT AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id GROUP BY reviews.review_id ORDER BY created_at  DESC")
     .then((results) => {
-        console.log(results.rows)
 
     const reviews = results.rows
 
