f911bb13e945a2f2123bbf982585aeecdc1f3344
Chris9533 <christopherbrownhill1995@gmail.com>
2022-05-20 16:33:20 +0100

    added 18. PATCH /api/comments/:comment_id and error handling

--- routes/comments-router.js
+++ routes/comments-router.js
@@ -1,6 +1,8 @@
 const commentsRouter = require('express').Router();
-const { removeComment } = require("../Controllers/comments.controllers");
+const { removeComment, patchComment } = require("../Controllers/comments.controllers");
 
 commentsRouter.delete("/:comment_id", removeComment);
+
+commentsRouter.patch("/:comment_id", patchComment )
 
 module.exports = commentsRouter
